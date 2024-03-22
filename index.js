9adf6611c21cf319ae97dfb8abf00ec187a0a075
valeria wabwire <larriebliss@gmail.com>
2024-03-22 10:02:25 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let num1= 31
+let num2= 2
+var multiply= 62;
+function multiply(num1, num2 ){
+    return num1 * num2;
+}
 
-// Write your code here
+var random= Math.floor(Math.random() * 100);
+
+let num3= 11
+var num4= 7
+let remainder= num3 % num4;
+var mod= 4;
+let max= 20
+let num5= 20
+let num6 =10
+function max( num5, num6){
+    if(20>2){
+        return 20;}
+    
+}
